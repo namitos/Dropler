da68dbd8659946a0a79a176005d562ef0de97f4e
Artem <namitos@users.noreply.github.com>
2016-03-09 21:46:10 +0300

    added base64 upload

--- dropler/plugin.js
+++ dropler/plugin.js
@@ -1,146 +1,169 @@
-CKEDITOR.plugins.add( 'dropler', {
-    init: function( editor ) {
-        backends = {
-            imgur: {
-                upload: uploadImgur,
-                required: ['clientId'],
-                init: function() {}
-            },
-            s3: {
-                upload: uploadS3,
-                required: [
-                    'bucket', 'accessKeyId','secretAccessKey', 'region'
-                ],
-                init: function() {
-                    var script = document.createElement('script');
-                    script.async = 1;
-                    script.src = 'https://sdk.amazonaws.com/js/aws-sdk-2.1.26.min.js';
-                    document.body.appendChild(script);
-                }
-            },
-            basic: {
-                upload: uploadBasic,
-                required: ['uploadUrl'],
-                init: function() {}
-            }
-        };
+CKEDITOR.plugins.add('dropler', {
+	init: function (editor) {
+		backends = {
+			imgur: {
+				upload: uploadImgur,
+				required: ['clientId'],
+				init: function () {
+				}
+			},
+			s3: {
+				upload: uploadS3,
+				required: [
+					'bucket', 'accessKeyId', 'secretAccessKey', 'region'
+				],
+				init: function () {
+					var script = document.createElement('script');
+					script.async = 1;
+					script.src = 'https://sdk.amazonaws.com/js/aws-sdk-2.1.26.min.js';
+					document.body.appendChild(script);
+				}
+			},
+			basic: {
+				upload: uploadBasic,
+				required: ['uploadUrl'],
+				init: function () {
+				}
+			},
+			base64: {
+				upload: uploadBase64,
+				required: [],
+				init: function () {
+				}
+			}
+		};
 
-        var checkRequirement = function(condition, message) {
-            if (!condition)
-                throw Error("Assert failed" + (typeof message !== "undefined" ? ": " + message : ""));
-        };
+		var checkRequirement = function (condition, message) {
+			if (!condition)
+				throw Error("Assert failed" + (typeof message !== "undefined" ? ": " + message : ""));
+		};
 
-        function validateConfig() {
-            var errorTemplate = 'DragDropUpload Error: ->';
-            checkRequirement(
-                editor.config.hasOwnProperty('droplerConfig'),
-                errorTemplate + "Missing required droplerConfig in CKEDITOR.config.js"
-            );
+		function validateConfig() {
+			var errorTemplate = 'DragDropUpload Error: ->';
+			checkRequirement(
+				editor.config.hasOwnProperty('droplerConfig'),
+				errorTemplate + "Missing required droplerConfig in CKEDITOR.config.js"
+			);
 
-            var backend = backends[editor.config.droplerConfig.backend];
+			var backend = backends[editor.config.droplerConfig.backend];
 
-            var suppliedKeys = Object.keys(editor.config.droplerConfig.settings);
-            var requiredKeys = backend.required;
+			var suppliedKeys = Object.keys(editor.config.droplerConfig.settings);
+			var requiredKeys = backend.required;
 
-            var missing = requiredKeys.filter(function(key) {
-                return suppliedKeys.indexOf(key) < 0
-            });
+			var missing = requiredKeys.filter(function (key) {
+				return suppliedKeys.indexOf(key) < 0
+			});
 
-            if (missing.length > 0) {
-                throw 'Invalid Config: Missing required keys: ' + missing.join(', ')
-            }
-        }
+			if (missing.length > 0) {
+				throw 'Invalid Config: Missing required keys: ' + missing.join(', ')
+			}
+		}
 
-        validateConfig();
+		validateConfig();
 
-        var backend = backends[editor.config.droplerConfig.backend];
-        backend.init();
+		var backend = backends[editor.config.droplerConfig.backend];
+		backend.init();
 
-        function doNothing(e) { }
-        function orPopError(err) { alert(err.data.error) }
+		function doNothing(e) {
+		}
 
-        function dropHandler(e) {
-            e.preventDefault();
-            var file = e.dataTransfer.files[0];
-            backend.upload(file).then(insertImage, orPopError);
-        }
+		function orPopError(err) {
+			alert(err.data.error)
+		}
 
-        function insertImage(href) {
-            var elem = editor.document.createElement('img', {
-                attributes: {
-                    src: href
-                }
-            });
-            editor.insertElement(elem);
-        }
+		function dropHandler(e) {
+			e.preventDefault();
+			var file = e.dataTransfer.files[0];
+			backend.upload(file).then(insertImage, orPopError);
+		}
 
-        function addHeaders(xhttp, headers) {
-            for (var key in headers) {
-                if (headers.hasOwnProperty(key)) {
-                    xhttp.setRequestHeader(key, headers[key]);
-                }
-            }
-        }
+		function insertImage(href) {
+			var elem = editor.document.createElement('img', {
+				attributes: {
+					src: href
+				}
+			});
+			editor.insertElement(elem);
+		}
 
-        function post(url, data, headers) {
-            return new Promise(function(resolve, reject) {
-                var xhttp    = new XMLHttpRequest();
-                xhttp.open('POST', url);
-                addHeaders(xhttp, headers);
-                xhttp.onreadystatechange = function () {
-                    if (xhttp.readyState === 4) {
-                        if (xhttp.status === 200) {
-                            resolve(JSON.parse(xhttp.responseText).data.link);
-                        } else {
-                            reject(JSON.parse(xhttp.responseText));
-                        }
-                    }
-                };
-                xhttp.send(data);
-            });
-        }
+		function addHeaders(xhttp, headers) {
+			for (var key in headers) {
+				if (headers.hasOwnProperty(key)) {
+					xhttp.setRequestHeader(key, headers[key]);
+				}
+			}
+		}
 
-        function uploadBasic(file) {
-            var settings = editor.config.droplerConfig.settings;
-            return post(settings.uploadUrl, file, settings.headers);
-        }
+		function post(url, data, headers) {
+			return new Promise(function (resolve, reject) {
+				var xhttp = new XMLHttpRequest();
+				xhttp.open('POST', url);
+				addHeaders(xhttp, headers);
+				xhttp.onreadystatechange = function () {
+					if (xhttp.readyState === 4) {
+						if (xhttp.status === 200) {
+							resolve(JSON.parse(xhttp.responseText).data.link);
+						} else {
+							reject(JSON.parse(xhttp.responseText));
+						}
+					}
+				};
+				xhttp.send(data);
+			});
+		}
 
-        function uploadImgur(file) {
-            var settings = editor.config.droplerConfig.settings;
-            return post('https://api.imgur.com/3/image', file, {'Authorization': settings.clientId});
-        }
+		function uploadBasic(file) {
+			var settings = editor.config.droplerConfig.settings;
+			return post(settings.uploadUrl, file, settings.headers);
+		}
 
-        function uploadS3(file) {
-            var settings = editor.config.droplerConfig.settings;
-            AWS.config.update({accessKeyId: settings.accessKeyId, secretAccessKey: settings.secretAccessKey});
-            AWS.config.region = 'us-east-1';
+		function uploadImgur(file) {
+			var settings = editor.config.droplerConfig.settings;
+			return post('https://api.imgur.com/3/image', file, {'Authorization': settings.clientId});
+		}
 
-            var bucket = new AWS.S3({params: {Bucket: settings.bucket}});
-            var params = {Key: file.name, ContentType: file.type, Body: file, ACL: "public-read"};
-            return new Promise(function(resolve, reject) {
-                bucket.upload(params, function (err, data) {
-                    if (!err) resolve(data.Location);
-                    else reject(err);
-                });
-            });
-        };
+		function uploadS3(file) {
+			var settings = editor.config.droplerConfig.settings;
+			AWS.config.update({accessKeyId: settings.accessKeyId, secretAccessKey: settings.secretAccessKey});
+			AWS.config.region = 'us-east-1';
 
-        CKEDITOR.on('instanceReady', function() {
-            var iframeBase = document.querySelector('iframe').contentDocument.querySelector('html');
-            var iframeBody = iframeBase.querySelector('body');
+			var bucket = new AWS.S3({params: {Bucket: settings.bucket}});
+			var params = {Key: file.name, ContentType: file.type, Body: file, ACL: "public-read"};
+			return new Promise(function (resolve, reject) {
+				bucket.upload(params, function (err, data) {
+					if (!err) resolve(data.Location);
+					else reject(err);
+				});
+			});
+		}
 
-            iframeBody.ondragover = doNothing;
-            iframeBody.ondrop = dropHandler;
+		function uploadBase64(file) {
+			console.log(file);
+			return new Promise(function (resolve, reject) {
+				var reader = new FileReader();
+				reader.onload = function (a) {
+					resolve(a.target.result);
+				};
+				reader.readAsDataURL(file);
+			});
+		}
 
-            paddingToCenterBody = ((iframeBase.offsetWidth - iframeBody.offsetWidth) / 2) + 'px';
-            iframeBase.style.height = '100%';
-            iframeBase.style.width = '100%';
-            iframeBase.style.overflowX = 'hidden';
+		CKEDITOR.on('instanceReady', function () {
+			var iframeBase = document.querySelector('iframe').contentDocument.querySelector('html');
+			var iframeBody = iframeBase.querySelector('body');
 
-            iframeBody.style.height = '100%';
-            iframeBody.style.margin = '0';
-            iframeBody.style.paddingLeft = paddingToCenterBody;
-            iframeBody.style.paddingRight = paddingToCenterBody;
-        });
-    }
+			iframeBody.ondragover = doNothing;
+			iframeBody.ondrop = dropHandler;
+
+			paddingToCenterBody = ((iframeBase.offsetWidth - iframeBody.offsetWidth) / 2) + 'px';
+			iframeBase.style.height = '100%';
+			iframeBase.style.width = '100%';
+			iframeBase.style.overflowX = 'hidden';
+
+			iframeBody.style.height = '100%';
+			iframeBody.style.margin = '0';
+			iframeBody.style.paddingLeft = paddingToCenterBody;
+			iframeBody.style.paddingRight = paddingToCenterBody;
+		});
+	}
 });
